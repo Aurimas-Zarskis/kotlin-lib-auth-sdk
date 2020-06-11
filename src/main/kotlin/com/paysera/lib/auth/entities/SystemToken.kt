9256268d754853e170934ebfc915b184e2bd2136
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-06-11 14:00:49 +0300

    Migrated to moshi

--- src/main/kotlin/com/paysera/lib/auth/entities/SystemToken.kt
+++ src/main/kotlin/com/paysera/lib/auth/entities/SystemToken.kt
@@ -1,7 +1,12 @@
 package com.paysera.lib.auth.entities
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class SystemToken(
     val value: String,
+    @Json(name = "expires_in")
     val expiresIn: Int,
     val scope: String,
     val audience: String
--- src/main/kotlin/com/paysera/lib/auth/entities/requests/CreateSystemTokenOptionalRequest.kt
+++ src/main/kotlin/com/paysera/lib/auth/entities/requests/CreateSystemTokenOptionalRequest.kt
@@ -1,5 +1,8 @@
 package com.paysera.lib.auth.entities.requests
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CreateSystemTokenOptionalRequest(
     val audience: String,
     val scope: String
